eb7125a88e5f8721615087863ae2e385f9ca2a9a
Jaimeson Waugh <jaimeson@4cesi.com>
2018-12-13 12:49:24 -0800

    Add theme option

--- src/WPThemeOption.php
+++ src/WPThemeOption.php
@@ -29,7 +29,7 @@ namespace WaughJ\WPThemeOption
 
 		public function render() : void
 		{
-			?><input type="text" id="<?= $this->slug; ?>" name="<?= $this->page->getOptionsGroup(); ?>[<?= $this->slug; ?>]" /><?php
+			?><input type="text" id="<?= $this->slug; ?>" name="<?= $this->page->getOptionsGroup(); ?>[<?= $this->slug; ?>]" placeholder="<?= $this->name; ?>" /><?php
 		}
 
 		private function getOptionValue()
