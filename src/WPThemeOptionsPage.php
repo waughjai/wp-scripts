390cde4d40ad8770708a9aef234fec920bfbe0d7
Jaimeson Waugh <jaimeson@4cesi.com>
2018-12-13 12:41:24 -0800

    Add theme option

--- src/WPThemeOptionsPage.php
+++ src/WPThemeOptionsPage.php
@@ -52,8 +52,6 @@ namespace WaughJ\WPThemeOption
 					<h1><?= $this->name; ?></h1>
 					<?php settings_errors(); ?>
 					<form method="post" action="options.php">
-						<?php settings_fields( $this->getOptionsGroup() ); ?>
-						<?php do_settings_sections( $this->getOptionsGroup() ); ?>
 						<?php submit_button(); ?>
 					</form>
 				</div>
